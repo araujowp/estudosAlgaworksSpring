8810a02d53f8f5e415fa96c81bf255bce2b33bea
Wagner <araujowp@gmail.com>
2019-05-05 20:21:56 -0300

    erro cap 3.10

--- src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
@@ -1,10 +1,22 @@
 package com.algaworks.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.algamoney.api.model.Categoria;
 import com.algaworks.algamoney.api.repository.CategoriaRepository;
@@ -13,6 +25,7 @@ import com.algaworks.algamoney.api.repository.CategoriaRepository;
 @RequestMapping("/categorias")
 public class CategoriaResource {
 	
+	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
 	@GetMapping
@@ -20,5 +33,31 @@ public class CategoriaResource {
 		return categoriaRepository.findAll();
 	}
 	
+	@PostMapping
+	//@ResponseStatus(HttpStatus.CREATED) ja estamos passando o status 
+	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
+		
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+		
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria>  buscarPeloCodigo(@PathVariable Long codigo) {
+		
+		Categoria categoriaEncontrada = categoriaRepository.findOne(codigo);
+		
+		if (categoriaEncontrada == null) {
+			return ResponseEntity.notFound().build();
+		}else {
+			return ResponseEntity.ok(categoriaEncontrada);
+		} 
 
+	}
+	
 }
