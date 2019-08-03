758aeb1bc111ef1df3a9c53469875dec805f62b6
Wagner <araujowp@gmail.com>
2019-08-03 16:06:21 -0300

    6.12. Adicionando permissões de acesso

--- src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
@@ -9,14 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algamoney.api.event.RecursoCriadoEvent;
@@ -26,54 +24,32 @@ import com.algaworks.algamoney.api.repository.CategoriaRepository;
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
 	@Autowired
-	private ApplicationEventPublisher publisher; 	
+	private ApplicationEventPublisher publisher;
 	
-	@CrossOrigin(maxAge = 10, origins = {"http://localhost:8000"})
 	@GetMapping
-	public List<Categoria> listar(){
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_CATEGORIA') and #oauth2.hasScope('read')")
+	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
 	}
 	
 	@PostMapping
-	//@ResponseStatus(HttpStatus.CREATED) ja estamos passando o status 
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_CATEGORIA') and #oauth2.hasScope('write')")
 	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
-		
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
-		
-		/*
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
-		*/
-		
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
-		
 		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
-		
-	}
-	
-	@DeleteMapping("/{codigo}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void remover(@PathVariable Long codigo) {
-		categoriaRepository.delete(codigo);
 	}
 	
 	@GetMapping("/{codigo}")
-	public ResponseEntity<Categoria>  buscarPeloCodigo(@PathVariable Long codigo) {
-		
-		Categoria categoriaEncontrada = categoriaRepository.findOne(codigo);
-		
-		if (categoriaEncontrada == null) {
-			return ResponseEntity.notFound().build();
-		}else {
-			return ResponseEntity.ok(categoriaEncontrada);
-		} 
-
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_CATEGORIA') and #oauth2.hasScope('read')")
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		 Categoria categoria = categoriaRepository.findOne(codigo);
+		 return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
 	}
 	
 }
