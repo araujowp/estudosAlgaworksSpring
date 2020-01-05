b16eb430ce6223672aa0c738e89a25fba42e656b
Wagner <araujowp@gmail.com>
2020-01-05 12:19:22 -0300

    7.1. Implementando projeção de lançamento

--- src/main/java/com/algaworks/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/LancamentoResource.java
@@ -29,6 +29,7 @@ import com.algaworks.algamoney.api.exceptionhandler.Erro;
 import com.algaworks.algamoney.api.model.Lancamento;
 import com.algaworks.algamoney.api.repository.LancamentoRepository;
 import com.algaworks.algamoney.api.repository.filter.LancamentoFilter;
+import com.algaworks.algamoney.api.repository.projection.ResumoLancamento;
 import com.algaworks.algamoney.api.service.LancamentoService;
 import com.algaworks.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
 
@@ -52,6 +53,11 @@ public class LancamentoResource {
 	public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable){
 		return lancamentoRepository.filtrar(lancamentoFilter, pageable);
 	}
+
+	@GetMapping(params  = "resumo")
+	public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable){
+		return lancamentoRepository.resumir(lancamentoFilter, pageable);
+	}	
 	
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Lancamento> buscaPeloCodigo(@PathVariable  Long codigo){
