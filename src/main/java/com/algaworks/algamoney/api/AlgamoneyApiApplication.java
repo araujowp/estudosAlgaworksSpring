6ccd3bb433d6e49dfc5c6a52e26a8f1e7e0726af
Wagner <araujowp@gmail.com>
2020-02-25 11:23:29 -0300

    7.2. Profiles do Spring

--- src/main/java/com/algaworks/algamoney/api/AlgamoneyApiApplication.java
+++ src/main/java/com/algaworks/algamoney/api/AlgamoneyApiApplication.java
@@ -2,8 +2,12 @@ package com.algaworks.algamoney.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.algaworks.algamoney.api.config.propertity.AlgamoneyApiPropertity;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AlgamoneyApiPropertity.class)
 public class AlgamoneyApiApplication {
 
 	public static void main(String[] args) {
